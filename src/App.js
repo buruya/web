996625a6fb93abd646f29d8ebfe6611c14662203
buruya <schiavonmatteomail@gmail.com>
2020-12-13 14:45:19 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { useWindowSize } from "@react-hook/window-size/throttled";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [width, height] = useWindowSize ( { fps : 60 });
   return (
     <div>
-      <iframe src="https://editor.p5js.org/buruya/embed/NRYhowiAG" width="100%" height="100%"></iframe>
+      <iframe src="https://editor.p5js.org/buruya/embed/NRYhowiAG" width={width} height={height}></iframe>
     </div>
   );
 }
